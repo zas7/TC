acea557c29bc1dc93f5a6fe73104eed85bd7582b
Stephan Zahnerx <stephan.zahner@statistik.ji.zh.ch>
2021-12-03 17:39:22 +0100

    Directory-Struktur in git abgebildet und geändert

--- TransferClient_Start.bat
+++ TransferClient_Start.bat
@@ -5,7 +5,7 @@
 :: ***************************************************************************************************
 ::
 :: Change the path to the java runtime vm to your needs (Variable JAVA_RT_PATH).
-:: The version must be at least 1.8.0_311
+:: The version must be at least 1.17.1
 ::
 :: ===================================================================================================
 
@@ -15,15 +15,15 @@ setlocal
 pushd %cd%
 
 :: change here
-set JAVA_RT_PATH=%cd%\tfc\bin\jre1.8.0_311
+set JAVA_RT_PATH=%cd%\bin\jre1.8.0_311
 
 set JAVAW="%JAVA_RT_PATH:"=%\bin\javaw.exe"
 
-start /b "" %JAVAW% -jar tfc\bin\transferClient.jar
+start /b "" %JAVAW% -jar bin\transferClient.jar
 
 if errorlevel 1 (
   echo.
-  echo Error occured at startup. Please inform support!
+  echo Error occured at startup. Please call your local support!
   echo.
   pause
   exit /b 1
