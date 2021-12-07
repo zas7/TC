4fc1a6381e715ea82f8be0397d26ae6b0d497fa2
Stephan Zahnerx <stephan.zahner@statistik.ji.zh.ch>
2021-12-07 11:58:58 +0100

    Build with JDK 17 and adding JRE

--- extractJre.bat
+++ extractJre.bat
@@ -1,14 +1,24 @@
 @echo on
 :: 
 :: Extracts a minimal JRE from JDK based on the necessary modules
-::
+:: Parameters:
+:: 1: JAVA_HOME
+:: 2: JENKINS WORKSPACE
 
+if [%1]==[] goto :usage
+if [%2]==[] goto :usage
 
-"%JAVA_HOME:"=%\bin\jlink.exe" ^
---module-path "%JAVA_HOME:"=%\bin\jmods" ^
---output "%WORKSPACE:"=%\target\jre" ^
+"%~1\bin\jlink.exe" ^
+--module-path "%~1\bin\jmods" ^
+--output "%~2\target\jre" ^
 --no-header-files ^
 --strip-debug ^
 --no-man-pages ^
---add-modules java.base,java.desktop,java.xml ^
-&& exit %ERRORLEVEL%
+--add-modules java.base,java.desktop,java.xml && ^
+exit %%ERRORLEVEL%%
+
+:usage
+echo Call with parameters
+echo 1: JAVA_HOME
+echo 2: JENKINS WORKSPACE
+exit / b1
